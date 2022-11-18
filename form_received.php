eed77d37a27d9a61c4be53433b457211284d5e60
Ruben Perez <ruben.prezjuarez@mail.mcgill.ca>
2022-11-17 22:01:08 -0500

    Initial commit

---
+++ form_received.php
@@ -0,0 +1,21 @@
+<?php
+
+echo "Thank you for your submission.";
+
+echo "<p>";
+echo $_POST["name"];
+echo "</p>";
+
+echo "<p>";
+echo $_POST["subjectForm"];
+echo "</p>";
+
+echo "<p>";
+echo $_POST["emailForm"];
+echo "</p>";
+
+echo "<p>";
+echo $_POST["messageForm"];
+echo "</p>";
+
+?>
